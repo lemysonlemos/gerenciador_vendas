77a6ef9a3e6863e918673f6bf24a5f62df05d4ca
lemyson <lemyson.lemos@lais.huol.ufrn.br>
2025-06-16 20:08:19 -0300

    fix: ajustes

--- gerenciador/settings.py
+++ gerenciador/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9*y-%z-0n9=!m35we3x#h4!+(k$!(l=k42hmec9jcik5*39lv*'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
